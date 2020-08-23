3bfeccece86ab72b62403ce869549b48c66ad924
Anewnoob <474225654@qq.com>
2020-08-23 20:28:42 +0800

    Create fastSort.py

---
+++ 面试题/fastSort.py
@@ -0,0 +1,19 @@
+问题描述：
+计算a ** n % b 其中a、b和n都是32位的非负整数
+即求a的n次方对b的余数
+问题示例：
+例如：2**31%3=2
+
+class Solution:
+    def fastPower(self, a, b, n):
+        ans = 1
+        while n > 0:
+            if n % 2 == 1:
+                ans = ans * a % b 
+            a = a * a % b
+            n = n / 2
+        return ans % b
+        
+ if __name__ == '__main__':
+    solution = Solution()
+    print(solution.fastPower(2, 31, 3))
